fee042d8d97010a66232a6bd112faae2128648b3
at <atxponh@gmail.com>
2024-12-17 20:45:34 -0500

    updated robotContainer to make arm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,9 +6,27 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.ArmIOSim;
+import frc.robot.subsystems.ArmIOSpark;
 
 public class RobotContainer {
+
+  Arm arm;
+
   public RobotContainer() {
+    switch(Constants.mode){
+      case REAL -> {
+        arm = new Arm(new ArmIOSpark());
+      }
+
+      case SIM -> {
+        arm = new Arm(new ArmIOSim());
+      }
+
+      case REPLAY -> {}
+    }
+
     configureBindings();
   }
 
